598697dc145f37a27441a73fadceec7f00147d5b
jamilsinno <jamil.sinno@gmail.com>
2023-03-20 20:34:56 -0300

    post request created

--- src/main/java/com/example/demo/student/StudentConfiguration.java
+++ src/main/java/com/example/demo/student/StudentConfiguration.java
@@ -16,14 +16,12 @@ public class StudentConfiguration {
             Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@gmail.com",
-                    LocalDate.of(2000, Month.JANUARY, 5),
-                    21
+                    LocalDate.of(2000, Month.JANUARY, 5)
             );
             Student alex = new Student(
                     "Alex",
                     "alex@gmail.com",
-                    LocalDate.of(2005, Month.JANUARY, 5),
-                    21
+                    LocalDate.of(2005, Month.JANUARY, 5)
             );
             repository.saveAll(
                     List.of(mariam, alex)
